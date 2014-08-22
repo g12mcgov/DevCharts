d3cd9c8feb8143ef02cc3f7c6098bd7fdb106afa
Grant McGovern <grant@cobrain.com>
2014-08-21 23:52:33 -0400

    Added more to script

--- dashboard.js
+++ dashboard.js
@@ -1,8 +1,55 @@
-console.log("hey");
 
-var secret = "hmpl57ur6fvh678";
-var client = new Dropbox.Client({key: "eym1lytwjtyifdo"});
-/*
+var client = new Dropbox.Client({key: '########', secret: '#########'});
+
+var token = "";
+var PATH = "/Developer";
+
+client.authenticate(function () {
+    if (client.isAuthenticated()) {
+		console.log("Dropbox has been authenticated");
+		token = client._oauth._token // Grab token returned
+	}
+	else {
+		alert("Login failed!");
+	} 
+});
+
+/* Get Account Info if we really want it */
+function accountInfo(){
+	client.getAccountInfo(function(error, accountInfo) {
+	if(error){
+		return showError(error);
+	}
+	
+	console.log(accountInfo);
+	
+	});
+};
+
+/* Get the contents from a diretory */
+function displayContents(directory_path){
+	client.readdir(directory_path, function(error, entries){
+		if(error){
+			var meta_url = "https://api.dropbox.com/1/metadata/auto/" + directory_path;
+			return showError(error);
+		}
+		else {
+			console.log(entries);
+		}
+	});
+};
+
+client.findByName(PATH, ".py", function(error, results){
+	if(error){
+		return showError(error);
+	}
+	else {
+		console.log(results.length);
+	}
+});
+
+//displayContents(PATH);
+
 (function poll(){
    setTimeout(function(){
       $.ajax({ url: "server", success: function(data){
@@ -15,4 +62,3 @@ var client = new Dropbox.Client({key: "eym1lytwjtyifdo"});
       }, dataType: "json"});
   }, 30000);
 })();
-*/
