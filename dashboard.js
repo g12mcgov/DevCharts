89f861e97250b2a4605c2c97bc78e094a1d2dd45
Grant McGovern <grant@cobrain.com>
2014-08-21 23:48:50 -0400

    First commit

---
+++ dashboard.js
@@ -0,0 +1,18 @@
+console.log("hey");
+
+var secret = "hmpl57ur6fvh678";
+var client = new Dropbox.Client({key: "eym1lytwjtyifdo"});
+/*
+(function poll(){
+   setTimeout(function(){
+      $.ajax({ url: "server", success: function(data){
+        //Update your dashboard gauge
+        salesGauge.setValue(data.value);
+
+
+        
+        poll();
+      }, dataType: "json"});
+  }, 30000);
+})();
+*/
